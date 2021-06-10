3ed39e2c778d88ea719e7530cc3126c33196c843
leon2401119 <leon2401119.cs04@nctu.edu.tw>
2021-06-10 14:49:23 +0800

    bug : augmented image cannot be decoded

--- plugin.cpp
+++ plugin.cpp
@@ -21,14 +21,36 @@ bool aug_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
     }
     return 0;
 }
-
+/*
 char* aug(UDF_INIT *const initid, UDF_ARGS *const args, char *const result, unsigned long *const length, char *const is_null, char *const error)
 {
+    
     char buff[50];
     snprintf(buff, sizeof(buff), "%ld\n", args->lengths[0]);
     strcpy(result,buff);
+    
+
+    Mat img = imdecode(args->args[0],CV_LOAD_IMAGE_COLOR);
 
     //strcpy(result,"Hello!");
     *length = 10;
+    return result;
+}
+*/
+
+char* aug(UDF_INIT *const initid, UDF_ARGS *const args, char *const result, unsigned long *const length, char *const is_null, char *const error)
+{
+    std::vector<char> buffer(args->args[0], args->args[0]+args->lengths[0]);
+    cv::Mat image = cv::imdecode(buffer, 2);
+    cv::Mat image_fliped;
+    cv::flip(image, image_fliped, 0);
+    std::vector<unsigned char> img_encoded;
+    std::vector<int> param = std::vector<int>(2);
+    param[0] = cv::IMWRITE_JPEG_QUALITY;
+    param[1] = 100;
+    cv::imencode(".png", image_fliped, img_encoded, param);
+    char *tmp = reinterpret_cast<char*>(img_encoded.data());
+    strcpy(result, tmp);
+    //*length = img_encoded.size();
     return result;
 }
