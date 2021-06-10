b8576b9b4676a2acceb8c7afaf0cb6e6e40464f7
leon2401119 <leon2401119.cs04@nctu.edu.tw>
2021-06-10 19:47:16 +0800

    almost done

--- plugin.cpp
+++ plugin.cpp
@@ -6,11 +6,12 @@
 using namespace cv;
 
 extern "C" {
-    bool aug_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
-    char *aug(UDF_INIT *const initid, UDF_ARGS *const args, char *const result, unsigned long *const length, char *const is_null, char *const error);
+    bool hflip_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+    char *hflip(UDF_INIT *const initid, UDF_ARGS *const args, char *const result, unsigned long *const length, char *const is_null, char *const error);
+    //void hflip_deinit(UDF_INIT *const initid);
 }
 
-bool aug_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
+bool hflip_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
     if(args->arg_count!=1){
     	strcpy(message,"usage:aug(img)\n");
 	return 1;
@@ -19,38 +20,38 @@ bool aug_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
 	strcpy(message,"arg[0] is not an image!\n");
 	return 1;
     }
+    initid->max_length = 16000000;
     return 0;
 }
-/*
-char* aug(UDF_INIT *const initid, UDF_ARGS *const args, char *const result, unsigned long *const length, char *const is_null, char *const error)
-{
-    
-    char buff[50];
-    snprintf(buff, sizeof(buff), "%ld\n", args->lengths[0]);
-    strcpy(result,buff);
-    
 
-    Mat img = imdecode(args->args[0],CV_LOAD_IMAGE_COLOR);
-
-    //strcpy(result,"Hello!");
-    *length = 10;
-    return result;
-}
-*/
-
-char* aug(UDF_INIT *const initid, UDF_ARGS *const args, char *const result, unsigned long *const length, char *const is_null, char *const error)
+char* hflip(UDF_INIT *const initid, UDF_ARGS *const args, char *const result, unsigned long *const length, char *const is_null, char *const error)
 {
     std::vector<char> buffer(args->args[0], args->args[0]+args->lengths[0]);
-    cv::Mat image = cv::imdecode(buffer, 2);
+    cv::Mat image = cv::imdecode(buffer, IMREAD_ANYCOLOR);
     cv::Mat image_fliped;
     cv::flip(image, image_fliped, 0);
+
+	// above doesn't cause error
+
     std::vector<unsigned char> img_encoded;
     std::vector<int> param = std::vector<int>(2);
-    param[0] = cv::IMWRITE_JPEG_QUALITY;
-    param[1] = 100;
+    param[0] = IMWRITE_PNG_COMPRESSION;
+    param[1] = 3;
+
+    	// above also error-free
+
     cv::imencode(".png", image_fliped, img_encoded, param);
     char *tmp = reinterpret_cast<char*>(img_encoded.data());
-    strcpy(result, tmp);
-    //*length = img_encoded.size();
-    return result;
+    
+    	// also no error	
+
+    char *r = new char[img_encoded.size()];
+    memcpy(r, tmp, img_encoded.size());
+    *length = (unsigned long)img_encoded.size();
+    return r;
 }
+/*
+void hflip_deinit(UDF_INIT *const initid){
+	
+}
+*/
